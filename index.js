f81aad571809310601e0ab80cafd5acabdb174e3
successobayuwana@gmail.com <successobayuwana@gmail.com>
2023-06-30 10:20:09 +0100

    NFT Preview Card Components

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(true || false)
+console.log(false || true)
+
+console.log("____")
+
+console.log(true && false)
+console.log(false && true)
+
+console.log("_____")
+console.log(true && true)
+console.log("____")
+console.log(false && false)
